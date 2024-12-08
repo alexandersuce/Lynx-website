7bec34d0da7c766fd6e35e6b184734b2cc17520e
Devnest-28 <143426420+alexandersuce@users.noreply.github.com>
2024-12-08 10:03:57 +0100

    Add files via upload

---
+++ site web/script.js
@@ -0,0 +1,16 @@
+// Redirection vers le projet GitHub pour "Open source"
+document.querySelector('a#downloadCode-Btn').addEventListener('click', function (event) {
+    event.preventDefault(); // Empêche le comportement par défaut
+    window.location.href = 'https://github.com/alexandersuce/Source-code-Lynx.git';
+});
+
+// Redirection vers le téléchargement du navigateur Lynx
+document.querySelector('button#downloadBtn').addEventListener('click', function () {
+    window.location.href = 'https://github.com/alexandersuce/Lynx-Browser.git';
+});
+
+// Ouverture de l'email prérempli
+document.querySelector('a[href*="gmail.com"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Empêche l'ouverture d'une page par défaut
+    window.location.href = 'mailto:luccabondi@gmail.com';
+});
